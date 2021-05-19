97ac9d9218c4720db0744c6642b0aae8c5f41cf2
gropmor <rari_atto@yahoo.co.jp>
2021-05-19 22:17:28 +0900

    Rails 課題1解答

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User
+  def initialize
+    @name = "mori"
+    @age = 21
+    @birthplace = "Tokyo"
+    @hobby = "piano"
+  end
+
+  def introduce
+    <<~EOS
+
+          私の名前は#{@name}です。
+      年齢は#{@age}歳。
+    出身地は#{@birthplace}で、趣味は#{@hobby}です。
+
+    EOS
+  end
+end
